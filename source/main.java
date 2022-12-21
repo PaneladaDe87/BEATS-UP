01c6d78f1218389a3d3812c53f39da7dc6ed64a4
PaneladaDe87 <102541192+PaneladaDe87@users.noreply.github.com>
2022-12-21 12:18:16 -0300

    Update main.java

--- source/main.java
+++ source/main.java
@@ -1,5 +1,10 @@
 package main;
 
-public static void main(String[] args) {
-    // lol nothing
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+    
 }
